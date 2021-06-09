d00e0748ccd90bf17246a2bdec5625273ae3e1df
rafi2011 <rnessa2011@gmail.com>
2021-06-08 23:59:15 -0400

    updated utils file

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-// ask questions
+function main() {
+
+    // ask questions
+    inquirer.prompt()
 
 
 //generate markdown from answers
 
 // save files
+
+}
+
+// start entry point
+main();
---
+++ utils/questions.js
@@ -0,0 +1,9 @@
+const questions = [
+    {
+        name: "",
+        message: "what's your repo name ?",
+
+    },
+
+];
+module.exports = questions
